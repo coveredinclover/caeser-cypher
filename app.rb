de470e95929805c087d2eda06eac164a1062eb7e
coveredinclover <rphale3000@gmail.com>
2022-09-18 09:32:27 -0600

    Commit caesar cypher

---
+++ app.rb
@@ -0,0 +1,16 @@
+# make function that takes a string and shifts it alphabetically down as many times
+# as the modifier specifies
+
+# check if the string is capitalized by setting a variable to true if needed
+
+def caesar_cypher(string, modifier = nil)
+  array1 = string.chars.map{|x| x.ord}
+  char_array = array1.map{|x| (x >= 97 && x <= 122) || (x >= 65 and x <= 90) ? x : x.chr}
+  wrap_array = char_array.map{|x| x.is_a?(Numeric) ? x + modifier : x}
+  wrapped_array = wrap_array.map{|x| x.is_a?(Numeric) && (x > 122 || (x > 90 && x < 97)) ? x - 26 : x}
+  final_array = wrapped_array.map{|x| x.chr}
+  p final_array.join('')
+end
+
+caesar_cypher('The quick brown fox jumps over the lazy dog', 1)
+
